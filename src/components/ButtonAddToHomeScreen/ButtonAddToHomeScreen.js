0f7b5d4bfca772145114dc8eba104e63582a7847
Sergey-Proskurnin <proskurnin@ua.fm>
2022-11-20 01:30:43 +0200

    Adds hook useAddToHomeScreenPrompt

--- src/components/ButtonAddToHomeScreen/ButtonAddToHomeScreen.js
+++ src/components/ButtonAddToHomeScreen/ButtonAddToHomeScreen.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 import s from './ButtonAddToHomeScreen.module.css';
 import icon from 'images/icon-app-phonebook.png';
 import ButtonCloseModal from 'components/ButtonCloseModal';
+import useAddToHomeScreenPrompt from 'hooks/useAddToHomeScreenPrompt';
 
 const useStyles = makeStyles(theme => ({
   customBtn: {
@@ -16,21 +17,44 @@ const useStyles = makeStyles(theme => ({
 
 const ButtonAddToHomeScreen = () => {
   const classes = useStyles();
+  const [prompt, promptToInstall] = useAddToHomeScreenPrompt();
+  const [isVisible, setVisibleState] = useState(false);
+
+  const hide = () => {
+    setVisibleState(true);
+    console.log(isVisible);
+  };
+
+  useEffect(() => {
+    if (prompt) {
+      setVisibleState(true);
+    }
+  }, [prompt]);
 
   return (
     <>
-      <Button
-        color="secondary"
-        variant="contained"
-        className={classes.customBtn}
-        style={{ position: 'absolute', bottom: '1%' }}
-      >
-        <img className={s.icon} src={icon} alt="Icon"></img>
-        Add to home screen
-        <ButtonCloseModal
-          style={{ position: 'absolute', top: '0%', right: '2%' }}
-        />
-      </Button>
+      {!isVisible && (
+        <div style={{ position: 'absolute', bottom: '1%' }}>
+          <Button
+            color="secondary"
+            variant="contained"
+            className={classes.customBtn}
+            onClick={promptToInstall}
+          >
+            <img className={s.icon} src={icon} alt="Icon"></img>
+            Add to home screen
+          </Button>
+          <ButtonCloseModal
+            style={{
+              position: 'absolute',
+              top: '2%',
+              right: '2%',
+              color: '#3f51b5',
+            }}
+            closeModal={hide}
+          />
+        </div>
+      )}
     </>
   );
 };
